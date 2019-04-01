b27f02d207d9f4dad3c8539cd8f9bc0a3e3ff8a8
Priti Gupta <43332924+pritispoc@users.noreply.github.com>
2019-04-01 11:04:56 +0200

    added own components

---
+++ src/components/Nyhetsartikel.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Nyhetsartikel() {
+  return (
+    <article>
+      <img src="https://source.unsplash.com/random/400x250/?cat" />
+      <h2>testing news articles</h2>
+      <a>läs mer..</a>
+    </article>
+  );
+}
+export default Nyhetsartikel;
---
+++ src/components/Nyhetslista.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nyhetsartikel from "./Nyhetsartikel";
+
+function Nyhetslista() {
+  return (
+    <section>
+      <Nyhetsartikel />
+      <Nyhetsartikel />
+      <Nyhetsartikel />
+      <Nyhetsartikel />
+    </section>
+  );
+}
+export default Nyhetslista;
